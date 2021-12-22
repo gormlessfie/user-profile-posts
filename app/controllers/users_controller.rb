c0a02fbdafe870f40a2208e69e4861f22deab41b
gormlessfie <gmlessfie@gmail.com>
2021-12-21 17:59:41 -0800

    Add bcrypt password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
   
     if @user.save
       @user.create_profile(name: 'John Smith', age: 13)
+      sessions[:current_user_id] = @user.id
       redirect_to users_path, notice: 'Account creation successful!'
     else
-      flash.now[:error] = 'Error! Unsuccessful!'
-      render :new
+      redirect_to users_path, notice: 'Error! Unsuccessful!'
     end
   end
 
