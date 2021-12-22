afda7309edcc2d78dfb767822c857323ce742f5b
gormlessfie <gmlessfie@gmail.com>
2021-12-21 17:47:21 -0800

    Add bcrypt and password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :username, presence: true
+  validates :password, presence: true, length: { in: 4..12 }
 
   has_one :profile, dependent: :destroy
   has_many :posts, dependent: :destroy
